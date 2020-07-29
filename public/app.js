937e78c0313169e3d55cafea7440ae9f242a1191
Tim Dockins <tdockins@spektronsystems.com>
2020-07-29 10:49:00 -0500

    angularized

--- public/app.js
+++ public/app.js
@@ -3,209 +3,134 @@
  * Created by tdockins on 11/6/2015.
  */
 'use strict';
-
+const URL = "//tigertrack.herokuapp.com/"
 /*
  * mongodb://heroku_k53fczgb:cg3goughamkpot76dal84f87uv@ds051534.mongolab.com:51534/heroku_k53fczgb
  */
 
-angular.module( 'sidamo', [
-  'ui.router',
-  'ui.bootstrap',
-  'ngResource'
-] )
-  .controller( 'NavigationCtrl', [
-    '$scope',
-    function ( $scope ) {
-      $scope.tree = {
-        data : [
-          {
-            name : "Shops",
-            link : "shops"
-          }
-        ]
-      };
-    }
-  ] )
-  .controller( 'MainCtrl', [
-    'SidamoShopService',
-    'SidamoDrinkService',
-    '$http',
-    function ( SidamoShopService, SidamoDrinkService, $http ) {
-      var vm = this;
+angular.module('tigertrack', [
+    'ui.router',
+    'ui.bootstrap',
+    'angularMoment',
+    'ngResource'
+])
+       .controller('NavigationCtrl', [
+           '$scope',
+           function ($scope) {
+               $scope.tree = {
+                   data : [
+                       {
+                           name : "Account",
+                           link : "account"
+                       },
+                       {
+                           name : "Track",
+                           link : "track"
+                       },
+                       {
+                           name : "Team",
+                           link : "team"
+                       }
+                   ]
+               };
+           }
+       ])
+       .controller('MainCtrl', [
+           'TigerAccountService',
+           '$http',
+           function (TigerAccountService, $http) {
+               const vm = this;
+               window.console && console.log(`MainCtrl defined`); //todo remove debug output
+
+           }
+       ])
+       .controller('TrackEditController', [
+           'TigerTrackService',
+           'moment',
+           '$http',
+           function (TigerTrackService, moment, $http) {
+               const vm = this;
+               window.console && console.log(`TrackEditController defined`); //todo remove debug output
+
+               vm.entryDate = moment().toDate();
+
+               vm.activityTemplates = [
+                   {
+                       name        : "emails",
+                       label       : "Emails",
+                       description : "The number of emails sent today",
+                       value       : 0
+                   },
+                   {
+                       name        : "phonecalls",
+                       label       : "Phone Calls",
+                       description : "The number of phone calls today",
+                       value       : 0
+                   },
+                   {
+                       name        : "textmessages",
+                       label       : "Text Messages",
+                       description : "The number of text messages sent today",
+                       value       : 0
+                   },
+                   {
+                       name        : "proposals",
+                       label       : "Proposals",
+                       description : "The number of proposals made today",
+                       value       : 0
+                   }
+               ]
 
 
+           }
+       ])
+       .factory('TigerAccountService', [
+           '$resource',
+           function ($resource) {
+               return $resource("//tigertrack.herokuapp.com/api/accounts/:accountId", {accountId : '@accountId'}, {})
+           }
+       ])
+       .factory('TigerTrackService', [
+           '$resource',
+           function ($resource) {
+               return $resource("//tigertrack.herokuapp.com/api/track/:trackId", {trackId : '@id'}, {});
+           }
+       ])
+       .config([
+                   '$stateProvider',
+                   '$locationProvider',
+                   '$urlRouterProvider',
+                   function ($stateProvider, $locationProvider, $urlRouterProvider) {
 
-      vm.state = 'unsub';
+                       // $locationProvider.html5Mode( {enabled : true } ).hashPrefix( '!' );
 
-      vm.shops = [];
-      vm.selectedShop = undefined;
-      vm.shopEntered = undefined;
+                       $urlRouterProvider.otherwise("/");
+                       window.console && console.log(`config`, $stateProvider, $locationProvider, $urlRouterProvider); //todo remove debug output
+                       $stateProvider
+                           .state('main', {
+                               url          : '/',
+                               controller   : 'MainCtrl',
+                               templateUrl  : 'modules/welcome/views/welcome.html',
+                               controllerAs : 'vm'
+                           })
+                           .state('track', {
+                               url          : '/track',
+                               controller   : 'TrackEditController',
+                               templateUrl  : 'modules/track/views/edit.html',
+                               controllerAs : 'vm'
+                           })
+                       // .state( 'tracks', {
+                       //   abstract : true,
+                       //   url      : '/tracks',
+                       //   template : '<ui-view/>'
+                       // } )
+                       // .state( 'data.shops', {
+                       //   url          : '/shops',
+                       //   controller   : 'ShopsCtrl',
+                       //   templateUrl  : 'modules/shops/views/shops.html',
+                       //   controllerAs : 'vm'
+                       // } )
+                       ;
 
-      SidamoShopService.query( function ( data ) {
-        window.console && console.log( data );
-        data.forEach( function ( shop ) {
-          vm.shops.push( shop );
-        } )
-      } );
-
-      vm.submitShop = function () {
-        if ( vm.selectedShop == undefined ) {
-          vm.selectedShop = {
-            name     : vm.shopEntered,
-            location : null
-          }
-          vm.state = 'nameSubmitted';
-        }
-        else {
-          vm.state = 'shopSubmitted';
-        }
-
-      }
-
-      vm.getLocation = function ( val ) {
-        return $http.get( '//maps.googleapis.com/maps/api/geocode/json', {
-          params : {
-            address : val,
-            sensor  : false
-          }
-        } ).then( function ( response ) {
-          return response.data.results.map( function ( item ) {
-            return item.formatted_address;
-          } );
-        } );
-      };
-
-      vm.location = undefined;
-
-      vm.typeaheadOnSelect = function ( $item, $model, $label ) {
-        window.console && console.log( 'typeaheadOnSelect = ', $item, $model, $label );
-        vm.selectedShop = $item;
-      }
-
-
-      vm.submitLocation = function ( location ) {
-        vm.selectedShop.location = location;
-        vm.state = 'shopSubmitted';
-      }
-
-      vm.submitEmail = function ( email ) {
-
-        var saveObject = angular.extend( {}, vm.selectedShop, { vote : {
-          email : email, timestamp : moment().format( 'YYYYMMDDHHmm' )
-        }} );
-
-        SidamoShopService.save( saveObject, function ( res ) {
-          window.console && console.log( 'saved: ', res );
-          vm.selectedShop = res.value;
-
-          SidamoShopService.drinks( { shopId : vm.selectedShop._id }, function ( data ) {
-            data.forEach( function ( drink ) {
-              vm.drinks.push( drink );
-            } );
-            vm.state = 'emailSubmitted';
-          } );
-        } );
-      };
-
-      vm.drinks = [];
-      vm.selectedDrink = undefined;
-      vm.drinkEntered = undefined;
-      vm.submitDrink = function () {
-
-        var shopId, drinkName;
-        shopId = vm.selectedShop._id;
-
-        if ( vm.selectedDrink == undefined ) {
-          drinkName = vm.drinkEntered;
-          window.console && console.log(' saving drink to shop ', shopId, 'drink name', drinkName );
-          SidamoDrinkService.save( { shopId : shopId, drinkName : drinkName }, function ( data ) {
-            window.console && console.log('saved drink: ', data);
-            vm.selectedDrink = data.value;
-          })
-        }
-      };
-
-      vm.typeaheadOnDrinkSelect = function( $item, $model, $label ) {
-        window.console && console.log( 'typeaheadOnDrinkSelect = ', $item, $model, $label );
-        vm.selectedDrink = $item;
-      }
-
-      vm.drinkExperience = undefined;
-
-      vm.submitExperience = function() {
-        var shopId = vm.selectedShop._id;
-
-        SidamoDrinkService.experience( {drink : drink, experience : experience}, function( data ) {
-          window.console && console.log('saved experience: ', data);
-        })
-
-      }
-
-    }
-  ] )
-  .controller( 'ShopsCtrl', [
-    'SidamoShopService',
-    function ( SidamoShopService ) {
-      var vm = this;
-
-      vm.shops = [];
-
-      SidamoShopService.query( function ( data ) {
-        window.console && console.log( data );
-        data.forEach( function ( shop ) {
-          vm.shops.push( shop );
-        } )
-      } );
-
-    }
-  ] )
-  .factory( 'SidamoDrinkService', [
-    '$resource',
-    function( $resource ) {
-      return $resource( "//sidamo.herokuapp.com/api/drinks/:drinkId", {drinkId : '@drinkId'}, {})
-    }
-  ])
-  .factory( 'SidamoShopService', [
-    '$resource',
-    function ( $resource ) {
-      return $resource( "//sidamo.herokuapp.com/api/shop/:shopId", {shopId : '@id'}, {
-        drinks : {
-          method  : 'GET',
-          url     : "//sidamo.herokuapp.com/api/shop/:shopId/drinks",
-          isArray : true
-        }
-      } );
-    }
-  ] )
-  .config( [
-    '$stateProvider',
-    '$locationProvider',
-    '$urlRouterProvider',
-    function ( $stateProvider, $locationProvider, $urlRouterProvider ) {
-
-      $locationProvider.html5Mode( {enabled : true } ).hashPrefix( '!' );
-
-      $urlRouterProvider.otherwise( "/" );
-
-      $stateProvider
-        .state( 'main', {
-          url          : '/',
-          controller   : 'MainCtrl',
-          templateUrl  : 'modules/welcome/views/welcome.html',
-          controllerAs : 'vm'
-        } )
-        .state( 'data', {
-          abstract : true,
-          url      : '/data',
-          template : '<ui-view/>'
-        } )
-        .state( 'data.shops', {
-          url          : '/shops',
-          controller   : 'ShopsCtrl',
-          templateUrl  : 'modules/shops/views/shops.html',
-          controllerAs : 'vm'
-        } )
-      ;
-
-    }
-  ] );
+                   }
+               ]);
